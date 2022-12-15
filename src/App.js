e2b5626e9d8d0bd90f3661e815fc44548e247fa1
stankezdenek <stankezdenek@seznam.cz>
2022-12-15 21:52:18 +0100

    prace

--- src/App.js
+++ src/App.js
@@ -3,8 +3,60 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-    <h1>Yo</h1>
+
+        <div className="graf1"> </div>
+        <div className="graf2"> </div>
+        <div className="graf3"> </div>
+        <div className="graf4"> </div>
+      <div className="hledani">
+
+      </div>
+      <div className="kcal">
+
+      <div className="kcal1">
+      </div>
+      </div>
+      <div className="bilk">
+          <div className="bilk1">
+          </div>
+      </div>
+      <div className="sach">
+          <div className="sach1">  </div>
+      </div>
+      <div className="tuk">
+          <div className="tuk1">  </div>
+      </div>
+
+    <div className="nabidka">
+
+
+      <div className="icons8-app">
+        </div>
+      <div className="kcalkalkul">Kcalkalkulačka</div>
+
+      <div className="novejcil">
+        <div className="novejcile">Novej cíl</div>
+      </div>
+      <div className="domov">
+        Domov
+      </div>
+      <div className="mojejidlo">
+        Moje jídlo
+      </div>
+      <div className="cviky">
+        Cviky
+      </div>
+    </div>
+
+
+
+
+
+
+
+
     </div>
   );
 }
